340480fb4a41345b825c3dc317db92a05791b4a1
Kangni Liu <kangni@faunarobotics.com>
2025-11-10 15:48:36 -0500

    init

---
+++ cmd.sh
@@ -0,0 +1,18 @@
+python scripts/smplx_to_robot.py --smplx_file  '/home/kangni/nas_mnt/motion_retarget/smplx_amass/ACCAD/Male2General_c3d/male2_Calibration_stageii.npz' --robot unitree_g1_with_hands --save_path '/home/kangni/nas_mnt/motion_retarget/GMR_retargeted_motion/unitree_g1_29dof_with_hands' --rate_limit --loop
+
+
+python scripts/smplx_to_robot.py --smplx_file   '/home/kangni/nas_mnt/motion_retarget/smplx_amass/ACCAD/Male2General_c3d/male2_Calibration_stageii.npz' --robot 27dof_v37_3_fullmesh --save_path '/home/kangni/nas_mnt/motion_retarget/GMR_retargeted_motion/27dof_v37_3_fullmesh' --rate_limit --loop
+
+
+
+python scripts/smplx_to_robot.py --smplx_file  '/home/kangni/nas_mnt/motion_retarget/smplx_amass/ACCAD/Male2General_c3d/male2_Calibration_stageii.npz' --robot alpha --save_path '/home/kangni/nas_mnt/motion_retarget/GMR_retargeted_motion/alpha' --rate_limit --loop
+
+
+
+
+python scripts/smplx_to_robot.py --smplx_file  '/home/kangni/nas_mnt/motion_retarget/smplx_amass/ACCAD/Male2MartialArtsExtended_c3d/Extended_1_stageii.npz' \
+ --robot alpha --save_path '/home/kangni/nas_mnt/motion_retarget/GMR_retargeted_motion/alpha' --rate_limit --loop
+
+
+python scripts/smplx_to_robot.py --smplx_file  '/home/kangni/nas_mnt/motion_retarget/smplx_amass/ACCAD/Male2MartialArtsExtended_c3d/Extended_1_stageii.npz' \
+ --robot 27dof_v37_3_fullmesh --save_path '/home/kangni/nas_mnt/motion_retarget/GMR_retargeted_motion/27dof_v37_3_fullmesh' --rate_limit --loop
